f8ec8e75ffd0d845151369713a9a9d5c38c5900c
SGL99 <76397345+SGL99@users.noreply.github.com>
2022-04-22 13:00:58 +0530

    Update python-driver-code.py

--- strings/n_inputs/python-driver-code.py
+++ strings/n_inputs/python-driver-code.py
@@ -1,7 +1,17 @@
 # Python Driver Code
 
 def solve(opening_time: str, duration: str) -> str:
-  # Your code goes here
+  n=list(map(int,input().split()))
+m=list(map(int,input().split()))
+a=n[0]+m[0]
+b=n[1]+m[1]
+if(a>24);
+c=a-24
+if(b>60)
+c+=1;
+d=b-60
+print(c,end="")
+print(d)
 
   return 
 
